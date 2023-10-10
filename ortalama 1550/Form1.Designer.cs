c014734e7c591ec69609e75ccf7bdbc04724a893
ilaydadaban00 <145451273+ilaydadaban00@users.noreply.github.com>
2023-10-10 16:43:02 +0300

    Add files via upload

---
+++ ortalama 1550/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace ortalama_1550
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblnot3 = new System.Windows.Forms.Label();
+            this.lblNot4 = new System.Windows.Forms.Label();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.txtNot1 = new System.Windows.Forms.TextBox();
+            this.txtNot2 = new System.Windows.Forms.TextBox();
+            this.txtNot3 = new System.Windows.Forms.TextBox();
+            this.txtNot4 = new System.Windows.Forms.TextBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "not1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "not2";
+            // 
+            // lblnot3
+            // 
+            this.lblnot3.AutoSize = true;
+            this.lblnot3.Location = new System.Drawing.Point(57, 109);
+            this.lblnot3.Name = "lblnot3";
+            this.lblnot3.Size = new System.Drawing.Size(28, 13);
+            this.lblnot3.TabIndex = 2;
+            this.lblnot3.Text = "not3";
+            // 
+            // lblNot4
+            // 
+            this.lblNot4.AutoSize = true;
+            this.lblNot4.Location = new System.Drawing.Point(57, 136);
+            this.lblNot4.Name = "lblNot4";
+            this.lblNot4.Size = new System.Drawing.Size(28, 13);
+            this.lblNot4.TabIndex = 3;
+            this.lblNot4.Text = "not4";
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.Location = new System.Drawing.Point(57, 226);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(65, 25);
+            this.lblSonuc.TabIndex = 4;
+            this.lblSonuc.Text = "sonuc";
+            // 
+            // txtNot1
+            // 
+            this.txtNot1.Location = new System.Drawing.Point(160, 49);
+            this.txtNot1.Name = "txtNot1";
+            this.txtNot1.Size = new System.Drawing.Size(100, 20);
+            this.txtNot1.TabIndex = 5;
+            // 
+            // txtNot2
+            // 
+            this.txtNot2.Location = new System.Drawing.Point(160, 80);
+            this.txtNot2.Name = "txtNot2";
+            this.txtNot2.Size = new System.Drawing.Size(100, 20);
+            this.txtNot2.TabIndex = 6;
+            // 
+            // txtNot3
+            // 
+            this.txtNot3.Location = new System.Drawing.Point(160, 109);
+            this.txtNot3.Name = "txtNot3";
+            this.txtNot3.Size = new System.Drawing.Size(100, 20);
+            this.txtNot3.TabIndex = 7;
+            // 
+            // txtNot4
+            // 
+            this.txtNot4.Location = new System.Drawing.Point(160, 136);
+            this.txtNot4.Name = "txtNot4";
+            this.txtNot4.Size = new System.Drawing.Size(100, 20);
+            this.txtNot4.TabIndex = 8;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(9, 306);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(251, 116);
+            this.btnHesapla.TabIndex = 9;
+            this.btnHesapla.Text = "hessapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 450);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.txtNot4);
+            this.Controls.Add(this.txtNot3);
+            this.Controls.Add(this.txtNot2);
+            this.Controls.Add(this.txtNot1);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.lblNot4);
+            this.Controls.Add(this.lblnot3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblnot3;
+        private System.Windows.Forms.Label lblNot4;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.TextBox txtNot1;
+        private System.Windows.Forms.TextBox txtNot2;
+        private System.Windows.Forms.TextBox txtNot3;
+        private System.Windows.Forms.TextBox txtNot4;
+        private System.Windows.Forms.Button btnHesapla;
+    }
+}
+
---
+++ ortalama 1550/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ortalama_1550
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            double not1, not2, not3, not4, sonuc;
+            not1 = Convert.ToDouble(txtNot1.Text);
+            not2 = Convert.ToDouble(txtNot2.Text);
+            not3 = Convert.ToDouble(txtNot3.Text);
+            not4 = Convert.ToDouble(txtNot4.Text);
+            sonuc = (not1 + not2 + not3 + not4) / 4;
+
+
+
+            if (sonuc < 50 )
+            {
+
+
+                lblSonuc.Text = "kaldı";
+            }
+
+           else  if (sonuc > 50 & sonuc< 70) 
+            {
+
+            
+            lblSonuc.Text = "geçtiniz";
+             }
+            else if (sonuc >= 70 & sonuc < 85)
+            {
+                lblSonuc.Text = "teşekkür";
+            }
+
+            else if (sonuc >= 85 & sonuc < 100)
+            {
+                lblSonuc.Text = "taktir";
+            }
+
+        }
+        
+    }
+}
